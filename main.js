65203b7eab307d2fe6f938c7bf48b986a54fa64f
BuildTools <unconfigured@null.spigotmc.org>
2019-08-28 21:34:56 +0200

    Ajout du fichier de configuration

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-const { Client } = require('discord.js');
+const { Client } = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
 const client = new Client({ disableEveryone: true });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+  console.log("Je suis prêt !");
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') msg.channel.send("Pong!");
-  if (msg.content === 'everyone') msg.channel.send("@everyone, salut à tous !", { disableEveryone: false });
-  if (msg.content === 'noteveryone') msg.channel.send("@everyone (noteveryone), salut à tous !");
+client.on("message", msg => {
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!");
 });
 
-client.login('NjE2MzM5OTU5Njk5MzQxMzkx.XWbJoQ.pGmZGWJ-KwD_oyWF-ozED54ruCo');
+client.login(TOKEN);
