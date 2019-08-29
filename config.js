3090550caba1d8db2c729be1982a134c3dc918d7
Kevin <34973659+keketiger@users.noreply.github.com>
2019-08-29 02:58:25 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    TOKEN: "token",
+    DBCONNECTION: "mongodb://localhost:27017/DataBase",
+    DEFAULTSETTINGS: {
+        prefix: "!",
+        welcomeChannel: "logs",
+        welcomeMessage: "Welcome {{user}} !"
+    }
+};
